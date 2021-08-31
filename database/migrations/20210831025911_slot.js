4d970374eabe38a282412a532244cc087e818ef3
Bryan Szendel <bryan@bigarmor.com>
2021-08-31 03:26:00 -0400

    update migration file

--- database/migrations/20210831025911_slot.js
+++ database/migrations/20210831025911_slot.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable("slot", (slot) => {
     slot.increments();
-    slot.string("person_name", 255).notNullable();
+    slot.string("person_name", 255);
     slot.integer("hour").notNullable();
     slot.integer("minutes").notNullable();
     slot.timestamps(); // will create two columns: created_at, updated_at
